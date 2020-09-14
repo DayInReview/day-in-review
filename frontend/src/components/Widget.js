0b0a6cbbf6001cb858b975ca089294f4a860f030
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2020-09-14 10:41:59 -0500

    Adds grid layout for different sized widget cards

--- frontend/src/components/Widget.js
+++ frontend/src/components/Widget.js
@@ -3,17 +3,16 @@ import './Widget.css';
 import {
   Card,
   CardActionArea,
-  CardActions,
   CardContent,
   CardMedia,
-  Typography
+  Typography,
+  Grid
 } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
 
 const styles = theme => ({
   root: {
-    maxWidth: 345
   },
   media: {
     height: 140
@@ -25,24 +24,43 @@ class Widget extends Component {
     const pageLinks = {
       "todo": "/todo",
     };
+    const cardSize = {
+      "small": {
+        "xs": 12,
+        "sm": 6,
+        "md": 3
+      },
+      "medium": {
+        "xs": 12,
+        "sm": 6,
+        "md": 6
+      },
+      "large": {
+        "xs": 12,
+        "sm": 12,
+        "md": 12
+      }
+    };
     const { classes } = this.props;
 
     return (
-      <Card className={classes.root}>
-        <CardActionArea onClick={() => {window.location.assign(pageLinks[this.props.widget])}}>
-          <CardMedia
-            className={classes.media}
-          />
-          <CardContent>
-            <Typography variant="h5">
-              {this.props.name}
-            </Typography>
-            <Typography>
-              Filler text here for now
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+      <Grid item xs={cardSize[this.props.size]["xs"]} sm={cardSize[this.props.size]["sm"]} md={cardSize[this.props.size]["md"]}>
+        <Card className={classes.root}>
+          <CardActionArea onClick={() => {window.location.assign(pageLinks[this.props.widget])}}>
+            <CardMedia
+              className={classes.media}
+            />
+            <CardContent>
+              <Typography variant="h5">
+                {this.props.name}
+              </Typography>
+              <Typography>
+                Filler text here for now
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
     );
   }
 }
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
 import './Dashboard.css';
 import Widget from '../components/Widget'
-
+import {
+  Grid
+} from '@material-ui/core'
 
 export default class Dashboard extends Component {
   render() {
     // Create widgets
     const widgets = [];
     for (let i = 0; i < 6; i++) {
-      widgets.push(<Widget name="Small Widget" widget="todo" />)
+      widgets.push(<Widget name="Small Widget" size="small" widget="todo" />)
     }
     for (let i = 0; i < 4; i++) {
-      widgets.push(<Widget name="Medium Widget" />)
+      widgets.push(<Widget name="Medium Widget" size="medium" />)
     }
     for (let i = 0; i < 2; i++) {
-      widgets.push(<Widget name="Large Widget" />)
+      widgets.push(<Widget name="Large Widget" size="large" />)
     }
 
     return (
       // Row of Widgets
-      <div className="row">
+      <Grid container spacing={2}>
         {widgets}
-      </div>
+      </Grid>
     );
   }
 }
