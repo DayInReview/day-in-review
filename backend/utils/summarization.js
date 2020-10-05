cae1b8a1537915ee64f02d32c6168d125417d07f
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2020-10-05 18:14:30 -0500

    Return proper value from summarize function

--- backend/utils/summarization.js
+++ backend/utils/summarization.js
@@ -2,7 +2,7 @@ const SummarizerManager = require("node-summarizer").SummarizerManager;
 
 function summarize (text) {
   let Summarizer = new SummarizerManager(text.body, text.numSentences);
-  Summarizer.getSummaryByRank().then(summary_obj => {
+  return Summarizer.getSummaryByRank().then(summary_obj => {
     return summary_obj.summary;
   });
 }
