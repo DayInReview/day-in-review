9473432c1e6508e70fa4fd3b84963d75c7bab659
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2020-10-11 13:36:46 -0500

    Removes config

--- server/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  database: {
-    user: "rponnekanti",
-    password: "WveknRWujLc6Ni7H"
-  },
-  secretOrKey: "q6mRSvP0GPVskZrdHqEznd40dZ3n5FTsEfeJARnoDwKLqZI9Wzw8UCFPNzgKxA2"
-}
