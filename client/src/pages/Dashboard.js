24f6452a0fd97b02731caeb1ba03ef219dc93512
Rishi Ponnekanti <rishiponnekanti@gmail.com>
2020-12-13 18:11:23 -0600

    Adds widget icon to dashboard for the Grades widget

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -6,6 +6,7 @@ import {
 import ListAltOutlinedIcon from '@material-ui/icons/ListAltOutlined';
 import EmailOutlinedIcon from '@material-ui/icons/EmailOutlined';
 import DescriptionOutlinedIcon from '@material-ui/icons/DescriptionOutlined';
+import SchoolOutlinedIcon from '@material-ui/icons/SchoolOutlined';
 import Widget from '../components/Widget'
 
 
@@ -13,7 +14,8 @@ export default function Dashboard() {
   const descriptions = {
     "todo": "A simple to-do list",
     "email": "AI-powered email summaries delivered straight to your inbox",
-    "summarizer": "Get summaries of all sorts of documents"
+    "summarizer": "Get summaries of all sorts of documents",
+    "grades": "Track your course assignments, grades, and GPA",
   }
 
   // Create widgets
@@ -22,7 +24,7 @@ export default function Dashboard() {
   widgets.push(<Widget name="To-do" description={descriptions.todo} widget="todo" icon={<ListAltOutlinedIcon style={{ fontSize: 180, color: 'green' }}/>} />);
   widgets.push(<Widget name="Email" description={descriptions.email} widget="email" icon={<EmailOutlinedIcon style={{ fontSize: 180, color: 'blue' }}/>} />);
   widgets.push(<Widget name="Summarizer" description={descriptions.summarizer} widget="summarizer" icon={<DescriptionOutlinedIcon style={{ fontSize: 180, color: 'red' }}/>} />);
-  widgets.push(<Widget name="To-do" description={descriptions.todo} widget="todo" icon={<ListAltOutlinedIcon style={{ fontSize: 180, color: 'green' }}/>} />);
+  widgets.push(<Widget name="Grades" description={descriptions.grades} widget="grades" icon={<SchoolOutlinedIcon style={{ fontSize: 180, color: 'orange' }}/>} />);
   widgets.push(<Widget name="To-do" description={descriptions.todo} widget="todo" icon={<ListAltOutlinedIcon style={{ fontSize: 180, color: 'green' }}/>} />);
   widgets.push(<Widget name="To-do" description={descriptions.todo} widget="todo" icon={<ListAltOutlinedIcon style={{ fontSize: 180, color: 'green' }}/>} />);
 
