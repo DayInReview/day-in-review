72a7abc44f7cd40a7e0634d6820ff087207c747c
Ishan Shah <ishan0102@utexas.edu>
2020-12-28 10:12:13 -0600

    React Hooks (#34)
    ### Problem
    React components are relatively outdated and are annoying to use because of convoluted state variables and functions like `componentDidMount`.
    
    ### Solution
    This PR introduces [hooks](https://reactjs.org/docs/hooks-intro.html), which are much more widely used than components. A few steps were taken to convert the components to hooks.
    
    1. Change class definitions to `export default function foo(props)` and remove `render` methods
    2. Use `makeStyles` instead of `withStyles`
    3. Remove all instances of `this`
    4. Replace all constructors with `const [state, setState] = useState("any state data goes here")` and use `setState("any state data goes here")` to modify state
    5. Change `componentDidMount` to `useEffect`
    
    ### Testing
    I tested these changes by making sure all of the components still rendered and that the functionality was still there. @JimothyGreene when you get a chance, double check that `TodoList` is still functioning as expected because that's where I made the most alterations since I had to change a bunch of `setState` functions.
    
    ### Notes
    I referenced a [Medium post](https://olinations.medium.com/10-steps-to-convert-a-react-class-component-to-a-functional-component-with-hooks-ab198e0fa139) to help me figure out how to make these changes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,19 +12,18 @@ import {
   Button
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 
 import Dashboard from './pages/Dashboard';
 import TodoList from './pages/TodoList';
 
-
-const styles = theme => ({
+const useStyles = makeStyles({
   root: {
     flexGrow: 1
   },
   menuButton: {
-    marginRight: theme.spacing(2)
+    marginRight: 2
   },
   title: {
     flexGrow: 1,
@@ -32,39 +31,36 @@ const styles = theme => ({
   },
   body: {
     textAlign: 'center'
-  }
+  },
 });
-class App extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <Router>
-        <AppBar className={classes.root} position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title} onClick={() => {window.location.assign('/')}}>
-              Day in Review
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-        {/* Main Body */}
-        <div className={classes.body}>
-          {/* Pages */}
-          <Switch>
-            <Route exact path="/">
-              <Dashboard />
-            </Route>
-            <Route exact path="/todo">
-              <TodoList />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
 
-export default withStyles(styles)(App);
+export default function App(props) {
+  const classes = useStyles(props);
+  return (
+    <Router>
+      <AppBar className={classes.root} position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title} onClick={() => {window.location.assign('/')}}>
+            Day in Review
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      {/* Main Body */}
+      <div className={classes.body}>
+        {/* Pages */}
+        <Switch>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>
+          <Route exact path="/todo">
+            <TodoList />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
