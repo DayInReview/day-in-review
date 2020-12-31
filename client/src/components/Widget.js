3b3d2c8735831a7a4cedcfb5c615ef2fc9fd2111
ishan0102 <ishan0102@gmail.com>
2020-12-30 22:55:31 -0600

    Set up dashboard with core widgets and icons

--- client/src/components/Widget.js
+++ client/src/components/Widget.js
@@ -10,51 +10,49 @@ import {
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-
 const useStyles = makeStyles({
   root: {
+    margin: '0.4rem',
+    borderRadius: '10px',
+    textAlign: 'center'
   },
   media: {
-    height: 140
-  }
+    height: '20vh'
+  },
+  content: {
+    height: '12vh',
+    padding: '2rem',
+    display: 'flex',
+    flexDirection: 'column'
+  },
 });
 
 export default function Widget(props) {
   const pageLinks = {
     "todo": "/todo",
+    "email": "/email",
+    "summarizer": "/summarizer"
   };
   const cardSize = {
-    "small": {
-      "xs": 12,
-      "sm": 6,
-      "md": 3
-    },
-    "medium": {
-      "xs": 12,
-      "sm": 6,
-      "md": 6
-    },
-    "large": {
-      "xs": 12,
-      "sm": 12,
-      "md": 12
-    }
+    "xs": 12,
+    "sm": 6,
+    "md": 4
   };
   const classes = useStyles(props);
 
   return (
-    <Grid item xs={cardSize[props.size]["xs"]} sm={cardSize[props.size]["sm"]} md={cardSize[props.size]["md"]}>
+    <Grid item xs={cardSize["xs"]} sm={cardSize["sm"]} md={cardSize["md"]}>
       <Card className={classes.root}>
-        <CardActionArea onClick={() => {window.location.assign(pageLinks[props.widget])}}>
-          <CardMedia
-            className={classes.media}
-          />
-          <CardContent>
-            <Typography variant="h5">
+        <CardActionArea onClick={() => { window.location.assign(pageLinks[props.widget]) }}>
+          <CardMedia id="card-media" className={classes.media}>
+            {props.icon}
+          </CardMedia>
+          <CardContent className={classes.content}>
+            <Typography variant="h3">
               {props.name}
             </Typography>
-            <Typography>
-              Filler text here for now
+            <Typography variant="h5">
+              {props.description}
             </Typography>
           </CardContent>
         </CardActionArea>
